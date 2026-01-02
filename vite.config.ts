925e5e4517119bb79c926b7392fb048ec32d2c54
ATTHIRAJU RAVITEJA <athirajuteja5@gmail.com>
2026-01-02 21:28:08 +0530

    Update Azure project live URL and fix image paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", 
+  base: "/My-Portfolio/",
   server: {
     host: "::",
     port: 8080,
